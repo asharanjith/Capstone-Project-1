a8b80ef455b96b8877e16dcd532f43b70691ea95
asharanjith <ashasvijayan@gmail.com>
2022-10-19 18:59:41 +0400

    Adapt speaker section to mobile view

--- speakercard.js
+++ speakercard.js
@@ -41,19 +41,24 @@ const speakerArray=[
 // Create a function to create a card
 
 const speakerSection = document.querySelector('#jsSection');
+const button= document.querySelector('.moreButton');
+let x=window.innerWidth;
+let num=2;
+if(x>768){
+    num=6;
+}
 
-for(let i=0;i<speakerArray.length;i+=1){
+
+for(let i=0;i<num;i+=1){
    speakerSection.innerHTML += `
    <div class="speaker">        
         <img src="${speakerArray[i].image}" alt="speaker1">
         <div class="speakerInfo">
             <h3>${speakerArray[i].name}</h3>
-            <p>${speakerArray[i].title}</p>
+            <p class="position">${speakerArray[i].title}</p>
             <p>${speakerArray[i].text}</p>
-        </div>
-                           
+        </div>      
         
-    </div>
-   
+    </div>   
    `
 }
